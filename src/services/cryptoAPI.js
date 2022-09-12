56cdc914adf3bd3f1badc16a5ab10fc5a5e7940f
Jason Liu <jhliu4@buffalo.edu>
2022-09-11 22:24:47 -0400

    git init

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,15 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       limit: '50',
+//       offset: '0',
+//       orderBy: '24hVolume',
+//       orderDirection: 'desc'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': '3f99899506mshb991c47410090dfp1cb9b1jsn57d6a92410bb',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
